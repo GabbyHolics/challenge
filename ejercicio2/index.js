19dc40add84dcc23176a99cecdcc4d5ace6f549b
GabbyHolics <karenthgabriela@gmail.com>
2021-12-22 22:00:05 -0300

    update

--- ejercicio2/index.js
+++ ejercicio2/index.js
@@ -22,4 +22,52 @@ const order= (values, json) =>{
     });
 
     return result;
+
 }
+
+let result = {
+    BUIN: {
+        limit: 1,
+        over: {
+            carrier: "CHP",
+            service: "express",
+        },
+        under: {
+            carrier: "CHP",
+            service: "express",
+        }
+    },
+    LAJA: {
+        limit: 1,
+        over: {
+            carrier: "CCH",
+            service: "DEX",
+        },
+        under: {
+             carrier: "CCH",
+            service: "DEX",
+        }
+    },
+    LEBU: {
+        limit: 1,
+        over: {
+            carrier: "CCH",
+            service: "DEX",
+        },
+        under: {
+             carrier: "CCH",
+            service: "DEX",
+        }
+    },
+    LOTA: {
+        limit: 1,
+        over: {
+            carrier: "CHP",
+            service: "express",
+        },
+        under: {
+            carrier: "CHP",
+            service: "express",
+        }
+    }
+} 
